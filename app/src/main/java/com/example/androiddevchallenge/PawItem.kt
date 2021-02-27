a3f963f7be7bdc81018ea1e7fbb96859b3c25e2d
kinnerapriyap <kinnerapriyap@gmail.com>
2021-02-27 13:57:50 +0900

    Add PawItem data class and parcelize

---
+++ app/src/main/java/com/example/androiddevchallenge/PawItem.kt
@@ -0,0 +1,10 @@
+package com.example.androiddevchallenge
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class PawItem(
+    val imageUrl: String,
+    val name: String
+) : Parcelable
