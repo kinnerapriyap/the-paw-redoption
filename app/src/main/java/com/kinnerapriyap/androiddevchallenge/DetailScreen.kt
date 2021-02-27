dfcf3be336f767b853993b542f260f3d4b7480f6
kinnerapriyap <kinnerapriyap@gmail.com>
2021-02-27 14:22:06 +0900

    Fix format

--- app/src/main/java/com/kinnerapriyap/androiddevchallenge/DetailScreen.kt
+++ app/src/main/java/com/kinnerapriyap/androiddevchallenge/DetailScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.kinnerapriyap.androiddevchallenge
 
 import androidx.compose.foundation.layout.Arrangement
@@ -6,12 +21,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.contentColorFor
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -49,9 +64,9 @@ fun DetailScreen(item: PawItem, navigateBack: () -> Unit) {
                 contentDescription = item.name,
                 requestBuilder = {
                     apply(RequestOptions().override(1000).centerCrop())
-                })
+                }
+            )
             Text(text = item.name, style = typography.h4)
-
         }
     }
 }
